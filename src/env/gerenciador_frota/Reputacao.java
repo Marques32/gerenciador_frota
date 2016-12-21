b4769021da8e0a3a20dc312437117772713b1e99
Marques32 <marques.nascimentoamaro@gmail.com>
2016-12-21 11:59:11 -0300

    Alterações

--- src/env/gerenciador_frota/Reputacao.java
+++ src/env/gerenciador_frota/Reputacao.java
@@ -1,37 +1,10 @@
 package gerenciador_frota;
 
 public class Reputacao {
-	private double reputacaoAnterior;
-	private double nivelIrresponsabilidade;
-	private double lembranca;
 	private int valor;
 	
 	public Reputacao(){
 		this.valor = 5;
-	}
-	
-	public double getReputacaoAnterior() {
-		return reputacaoAnterior;
-	}
-
-	public void setReputacaoAnterior(double reputacaoAnterior) {
-		this.reputacaoAnterior = reputacaoAnterior;
-	}
-
-	public double getNivelIrresponsabilidade() {
-		return nivelIrresponsabilidade;
-	}
-
-	public void setNivelIrresponsabilidade(double nivelIrresponsabilidade) {
-		this.nivelIrresponsabilidade = nivelIrresponsabilidade;
-	}
-
-	public double getLembranca() {
-		return lembranca;
-	}
-
-	public void setLembranca(double lembranca) {
-		this.lembranca = lembranca;
 	}
 
 	public int getValor() {
